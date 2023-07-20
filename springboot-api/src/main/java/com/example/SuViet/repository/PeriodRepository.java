cb00db9421a2440c1f57e231970e12dab5c286aa
goldenegghead <you@example.com>
2023-07-20 18:05:57 +0700

    detach spring boot project

--- springboot-api/src/main/java/com/example/SuViet/repository/PeriodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SuViet.repository;
-
-import com.example.SuViet.model.Period;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PeriodRepository extends JpaRepository<Period, Integer> {
-}
